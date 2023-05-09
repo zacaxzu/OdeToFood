58be9821993f17908dbd2c218616a7f65e0ab6a7
zacaxzu <zeljka.baca1@gmail.com>
2023-05-09 22:28:02 +0200

    Upload

---
+++ OdeToFood.Web/App_Start/ContainerConfig.cs
@@ -0,0 +1,22 @@
+﻿using Autofac;
+using OdeToFood.Data.Services;
+using System.Web.Mvc;
+
+namespace OdeToFood.Web
+{
+    public class ContainerConfig
+    {
+        internal static void RegisterContainer()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterControllers(typeof(MvcApplication).Assembly);
+            builder.RegisterType<InMemoryReastaurantData>()
+                   .As<IRestaurantData>()
+                   .SingleInstance();
+
+            var container = builder.Build();
+            DependencyResolver.SetResolver(new AutofacDependancyResolver(container));
+        }
+    }
+}
